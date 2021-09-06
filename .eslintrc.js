93fa13d3d1621047eed1a02fafaf57616ade1832
CaptainJack <captain_jack_mail@163.com>
2021-09-06 15:29:29 +0800

    test

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
-  parser: "babel-eslint",
-  plugins: ["prettier"],
-  "rules": {
-    'array-callback-return': 'off',
+  parser: 'babel-eslint',
+  plugins: ['prettier'],
+  rules: {
+    'array-callback-return': 'off'
   }
-};
+}
--- app.js
+++ app.js
@@ -1,4 +1,4 @@
 console.log('hello')
 console.log('simi')
 console.log('heloo')
-console.log('nice');
+console.log('nice')
